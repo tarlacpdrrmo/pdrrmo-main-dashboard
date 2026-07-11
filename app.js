8aac5ed60e9546e327d8e3588412a58042cf3d9b
AUTONOMOUS <39304727+tarlacpdrrmo@users.noreply.github.com>
2026-07-11 13:17:00 +0800

    Update Google Apps Script web app URL

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 Chart.register(ChartDataLabels);
 
 // 1. YOUR SECURE GOOGLE APPS SCRIPT WEB APP URL
-const webAppUrl = "https://script.google.com/macros/s/AKfycbwTk595HiAgDrIcZn-Bq-hG2wLrgmfsTA5ogt-0sT44J_OedeVG6xBfaZ_NePAw23cnjg/exec";
+const webAppUrl = "https://script.google.com/macros/s/AKfycbwYUt0YFQClUUXRGwrNdnC5INPXWzWyGUeN3J8E5tRKsO2ME-Y6zu5Fv0a56fCtxhwzTg/exec";
 // OPENWEATHERMAP CREDENTIALS
 const OWM_API_KEY = "3457c364d3f2840960216510c279837c"; 
 let rainChartInstance = null; 
